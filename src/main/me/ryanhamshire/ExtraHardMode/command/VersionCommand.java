19922e2b276d7cd71ff5d50ba52b5fecd0cc3155
max <di.em.ex@gmx.de>
2013-05-13 23:18:15 +0200

    Updated version command to include by jenkins dynamically created version number

--- src/main/me/ryanhamshire/ExtraHardMode/command/VersionCommand.java
+++ src/main/me/ryanhamshire/ExtraHardMode/command/VersionCommand.java
@@ -6,6 +6,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import javax.print.DocFlavor;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
 public class VersionCommand implements ICommand
 {
 
@@ -14,10 +21,36 @@ public class VersionCommand implements ICommand
     {
         sender.sendMessage(ChatColor.GRAY + "========= " + ChatColor.GOLD + plugin.getName() + ChatColor.GRAY + " =========");
         sender.sendMessage(ChatColor.BLUE + "Version: " + ChatColor.WHITE + plugin.getDescription().getVersion());
-        sender.sendMessage(ChatColor.BLUE + "Authors:");
-        for (String author : plugin.getDescription().getAuthors())
+        //Read buildnumber from manifest if availible
         {
-            sender.sendMessage(ChatColor.GRAY + " - " + ChatColor.WHITE + author);
+            JarFile pluginFile = null;
+            java.net.URL file =  plugin.getClass().getProtectionDomain().getCodeSource().getLocation();
+            try {
+                pluginFile = new JarFile(file.getFile());
+            } catch (IOException ignored) {}
+
+            if (pluginFile != null)
+            {
+                try {
+                    Manifest manifest = pluginFile.getManifest();
+                    if (manifest != null)
+                    {
+                        Object buildNumber = manifest.getMainAttributes().getValue("Build-Number");
+                        if (buildNumber instanceof String && ((String)buildNumber).length() > 0)
+                            sender.sendMessage(ChatColor.BLUE + "Build: " + ChatColor.WHITE + buildNumber);
+                    }
+                } catch (IOException ignored) {}
+            }
+        }
+
+        sender.sendMessage(ChatColor.BLUE + "Author:");
+        List<String> authors = plugin.getDescription().getAuthors();
+        sender.sendMessage(ChatColor.GRAY + " - " + ChatColor.WHITE + authors.get(0)); //author defined by "author: xyz" gets always loaded first
+
+        sender.sendMessage(ChatColor.BLUE + "Contributors:");
+        for (int i = 1; i < authors.size(); i++)
+        {
+            sender.sendMessage(ChatColor.GRAY + " - " + ChatColor.WHITE + authors.get(i));
         }
         return true;
     }
