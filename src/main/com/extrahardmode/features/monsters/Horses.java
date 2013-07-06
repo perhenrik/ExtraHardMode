e723ba0b1609ff271c9f8564bf849d430b5d6029
max <di.em.ex@gmx.de>
2013-07-06 18:48:22 +0200

    Nerf Horses

--- src/main/com/extrahardmode/features/monsters/Horses.java
+++ src/main/com/extrahardmode/features/monsters/Horses.java
@@ -24,11 +24,17 @@ package com.extrahardmode.features.monsters;
 
 import com.extrahardmode.ExtraHardMode;
 import com.extrahardmode.service.ListenerModule;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.HorseInventory;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * @author Diemex
@@ -41,12 +47,90 @@ public class Horses extends ListenerModule
     }
 
 
-    @EventHandler
-    public void onEntityInteract(InventoryOpenEvent event)
+    /**
+     * Block using of horse inventory in caves, prevent usage of horses as transportable chests
+     */
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onHorseInvClick(InventoryClickEvent event)
     {
         final Inventory inv = event.getInventory();
-        InventoryView view = event.getView();
-        Class clazz = event.getClass();
-        Player player = (Player) event.getPlayer();
+
+        //test
+        short dur = event.getCurrentItem().getDurability();
+        switch (event.getClick())
+        {
+            case RIGHT:
+                event.getCurrentItem().setDurability(dur--);
+                break;
+            case LEFT:
+                event.getCurrentItem().setDurability(dur++);
+                break;
+        }
+
+        plugin.getServer().broadcastMessage("durability: " + event.getCurrentItem().getDurability());
+
+        if (inv instanceof HorseInventory)
+        {
+            Inventory horseInv = event.getView().getTopInventory();
+            int clickedSlot = event.getRawSlot();
+
+            //TODO allow saddle and armor placement
+            //if (event.getWhoClicked().getLocation().getBlockY() < 60)
+            {
+                //In a horse inventory the first two slots are saddle + armor, a mule only has a saddle + potentially a chest
+                //Block usage of the chest in caves, but allow taking of the saddle
+                if (horseInv.getSize() > 2 && ((clickedSlot < horseInv.getSize() && clickedSlot > 0) || event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY))
+                {
+                    event.setCancelled(true);
+                }
+            }
+        }
     }
+
+
+    /**
+     * Greatly increase damage to horses
+     */
+    @EventHandler
+    public void onHorseDamage(EntityDamageEvent event)
+    {
+        if (event.getEntity() instanceof Horse)
+        {
+            final LivingEntity horse = (LivingEntity) event.getEntity();
+            final EntityDamageEvent.DamageCause cause = event.getCause();
+
+            switch (cause)
+            {
+                case BLOCK_EXPLOSION:
+                case ENTITY_EXPLOSION:
+                    //TODO EhmPlayerEnvironmentalDamageEvent for each type
+                    if (event.getDamage() > 2)
+                        horse.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20 * 15, 3));
+                    break;
+                case FALL:
+                    horse.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, (int) (20 * event.getDamage()), 4));
+                    event.setDamage(event.getDamage() * 2.0);
+                    break;
+                case SUFFOCATION:
+                    event.setDamage(event.getDamage() * 5.0);
+                    break;
+                case LAVA:
+                    event.setDamage(event.getDamage() * 2.0);
+                    break;
+                case FIRE_TICK:
+                    horse.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20, 1));
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Make horse armor wear out
+     */
+    /**
+     * Make saddle wear out
+     */
+    /**
+     * Make horses require food
+     */
 }
