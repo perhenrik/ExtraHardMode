e79b708e6e8fa68456f2fad9ed9e376119065b3c
max <di.em.ex@gmx.de>
2015-04-07 21:28:47 +0200

    Prevent Concurrent Modification

--- src/main/java/com/extrahardmode/task/WeightCheckTask.java
+++ src/main/java/com/extrahardmode/task/WeightCheckTask.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WeightCheckTask implements Runnable
 {
@@ -21,6 +23,7 @@ public class WeightCheckTask implements Runnable
     private final RootConfig CFG;
     private final MsgModule mMessenger;
     private static HashMap<UUID, Long> mLastClicks = new HashMap<UUID, Long>();
+    private static Lock lock = new ReentrantLock();
 
 
     public WeightCheckTask(ExtraHardMode plugin)
@@ -34,27 +37,36 @@ public class WeightCheckTask implements Runnable
     @Override
     public void run()
     {
-        for (UUID playerUuuid : mLastClicks.keySet())
+        if (lock.tryLock()) //Prevent concurrent modification
         {
-            Player player = mPlugin.getServer().getPlayer(playerUuuid);
-            //Remove players that haven't clicked in their inventory for 5 seconds
-            if (System.currentTimeMillis() - mLastClicks.get(playerUuuid) > 5000 || player == null)
+            try
             {
-                mLastClicks.remove(playerUuuid);
-                mMessenger.hidePopup(player, MsgCategory.WEIGHT_MSG.getUniqueIdentifier());
-            } else
+                for (UUID playerUuuid : mLastClicks.keySet())
+                {
+                    Player player = mPlugin.getServer().getPlayer(playerUuuid);
+                    //Remove players that haven't clicked in their inventory for 5 seconds
+                    if (System.currentTimeMillis() - mLastClicks.get(playerUuuid) > 5000 || player == null)
+                    {
+                        mLastClicks.remove(playerUuuid);
+                        mMessenger.hidePopup(player, MsgCategory.WEIGHT_MSG.getUniqueIdentifier());
+                    } else
+                    {
+                        final double armorPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_ARMOR_POINTS, player.getWorld().getName());
+                        final double invPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_INV_POINTS, player.getWorld().getName());
+                        final double toolPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_TOOL_POINTS, player.getWorld().getName());
+                        final double maxPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_MAX_POINTS, player.getWorld().getName());
+
+                        final float weight = PlayerModule.inventoryWeight(player, (float) armorPoints, (float) invPoints, (float) toolPoints);
+
+                        List<String> weightMessage = new ArrayList<String>(2);
+                        weightMessage.add(String.format("Weight %.1f/%.1f", weight, maxPoints));
+                        weightMessage.add(weight > maxPoints ? ChatColor.RED + "U will drown" : ChatColor.GREEN + "U won't drown");
+                        mMessenger.sendPopup(player, MsgCategory.WEIGHT_MSG, weightMessage, false);
+                    }
+                }
+            } finally
             {
-                final double armorPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_ARMOR_POINTS, player.getWorld().getName());
-                final double invPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_INV_POINTS, player.getWorld().getName());
-                final double toolPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_TOOL_POINTS, player.getWorld().getName());
-                final double maxPoints = CFG.getDouble(RootNode.NO_SWIMMING_IN_ARMOR_MAX_POINTS, player.getWorld().getName());
-
-                final float weight = PlayerModule.inventoryWeight(player, (float) armorPoints, (float) invPoints, (float) toolPoints);
-
-                List<String> weightMessage = new ArrayList<String>(2);
-                weightMessage.add(String.format("Weight %.1f/%.1f", weight, maxPoints));
-                weightMessage.add(weight > maxPoints ? ChatColor.RED + "U will drown" : ChatColor.GREEN + "U won't drown");
-                mMessenger.sendPopup(player, MsgCategory.WEIGHT_MSG, weightMessage, false);
+                lock.unlock();
             }
         }
     }
