36d1c26cbd861a40eafc78938e8c929e13d01231
Diemex <di.em.ex@gmx.de>
2016-02-11 20:09:51 +0200

    Update EnabledCommand to include all disabled and enabled worlds

--- src/main/java/com/extrahardmode/command/EnabledCommand.java
+++ src/main/java/com/extrahardmode/command/EnabledCommand.java
@@ -11,33 +11,31 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
-/** @author Diemex */
-public class EnabledCommand implements ICommand
-{
+/**
+ * @author Diemex
+ */
+public class EnabledCommand implements ICommand {
     @Override
-    public boolean execute(ExtraHardMode plugin, CommandSender sender, Command command, String label, String[] args)
-    {
-        if (sender.hasPermission(PermissionNode.ADMIN.getNode()))
-        {
+    public boolean execute(ExtraHardMode plugin, CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission(PermissionNode.ADMIN.getNode())) {
             World world = null;
-            if (sender instanceof Player)
-            {
+            if (sender instanceof Player) {
                 Player player = (Player) sender;
-                if (args.length > 0)
-                {
+                if (args.length > 0) {
                     world = plugin.getServer().getWorld(args[0]);
                     sender.sendMessage(String.format("A world named %s doesn't exist", args[0]));
                 }
                 if (world == null)
                     world = player.getWorld();
-            } else
-            {
+            } else {
                 if (args.length > 0)
                     world = plugin.getServer().getWorld(args[0]);
-                if (world == null)
-                {
+                if (world == null) {
                     sender.sendMessage(String.format("A world named %s doesn't exist", args[0]));
                     return false;
                 }
@@ -46,8 +44,19 @@ public class EnabledCommand implements ICommand
             boolean enabled = Arrays.asList(CFG.getEnabledWorlds()).contains(world.getName());
             if (CFG.isEnabledForAll())
                 sender.sendMessage(ChatColor.GREEN + "ExtraHardMode is enabled for all worlds");
-            else
-                sender.sendMessage(String.format("%s ExtraHardMode is %s in world %s", enabled ? ChatColor.GREEN : ChatColor.RED, enabled ? "enabled" : "disabled", world.getName()));
+            else {
+                if (CFG.getEnabledWorlds().length > 0)
+                    sender.sendMessage(String.format("%sEHM enabled for: %s", ChatColor.GREEN, Arrays.toString(CFG.getEnabledWorlds())));
+                //All this is for disabled worlds
+                List<String> enabledWorlds = Arrays.asList(CFG.getEnabledWorlds());
+                List<String> disabledWorlds = new ArrayList<String>();
+                for (World aWorld : plugin.getServer().getWorlds())
+                    disabledWorlds.add(aWorld.getName());
+                disabledWorlds.removeAll(enabledWorlds);
+                if (disabledWorlds.size() > 0)
+                    sender.sendMessage(String.format("%sEHM disabled for: %s", ChatColor.RED, disabledWorlds));
+                sender.sendMessage(String.format("%sCurrent world: %s %s", enabled ? ChatColor.GREEN : ChatColor.RED, world.getName(), enabled ? "enabled" : "disabled"));
+            }
         } else
             sender.sendMessage(ChatColor.RED + plugin.getTag() + " Lack permission: " + PermissionNode.ADMIN.getNode());
         return true;
